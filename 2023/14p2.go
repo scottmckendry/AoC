a29779fe3fa60ae055c59bcd018df3208cf7337e
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2023-12-15 17:30:13 +1300

    Gaming solution a bit on this one \_(ツ)_/

--- 2023/14p2.go
+++ 2023/14p2.go
@@ -7,34 +7,19 @@ import (
 )
 
 func D14P2() {
-	var rockCache = map[int]map[utils.Coordinate]rock{}
 	lines := utils.ReadLines("inputs/14.txt")
 	rocks := parseRocks(lines)
 
-	cycles := 1000000000
+	cycles := 1
 	directions := []rune{'N', 'W', 'S', 'E'}
 
-	patternFound := false
+	cycles = 160
 	for i := 0; i < cycles; i++ {
 		for _, direction := range directions {
 			rocks = rollRocks(rocks, len(lines[0]), len(lines), direction)
 		}
-		if !patternFound {
-			for cacheKey, cacheRocks := range rockCache {
-				isMatch := compareRocks(rocks, cacheRocks)
-				if isMatch {
-					patternFound = true
-					cycles = cycles % (i + 1 - cacheKey)
-				}
-			}
-		}
-
-		rockCache[i+1] = map[utils.Coordinate]rock{}
-		for coordinate, rock := range rocks {
-			rockCache[i+1][coordinate] = rock
-		}
 	}
-	rocks = rockCache[cycles]
+
 	totalLoad := calculateRockLoad(rocks, len(lines))
 	fmt.Println("Total load:", totalLoad)
 }
